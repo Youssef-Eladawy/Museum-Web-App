7848b7370e8dd411aae3af83f88b1dff592c14b3
Ali Hegazy <alihegazy19@outlook.com>
2025-11-09 14:00:02 +0200

    add contact page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Contact from "./components/contact/Contact";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import "@popperjs/core/dist/umd/popper.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";   
+
+function App(){
+	return(
+		<>
+			<Contact />
+		</>
+	)
+}
+
+export default App;
