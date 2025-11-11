dd519393f902a3c6cfb871abb13b9013934a5992
Ali Hegazy <alihegazy19@outlook.com>
2025-11-11 14:43:40 +0200

    Make the buttons belt in a component and share it in the destination

--- src/components/destination/Destination.jsx
+++ src/components/destination/Destination.jsx
@@ -1,5 +1,6 @@
 import HeadSection from "../shared/headSection/HeadSection";
 import SectionTitle from "../shared/sectionTitle/SectionTitle";
+import ButtonsBelt from "../shared/buttonsBelt/ButtosBelt";
 import "./Destination.css";
 // Photos
 import Canada from "./images/Canada2.jpg";
@@ -10,32 +11,6 @@ import China2 from "./images/China1.jpg";
 import Europe from "./images/Paris2.jpg";
 import Europe2 from "./images/Europe2.jpg";
 import Singapore from "./images/Singapore3.jpg";
-
-function show(e) {
-    let photos = document.querySelectorAll(`.photos .image`);
-    let lastActive = document.querySelector(`button.active`);
-
-    lastActive.classList.remove("active");
-    e.target.classList.add("active");
-
-    if (e.target.dataset.country === "all") {
-        for (const tag of photos) {
-            tag.classList.add("d-block");
-            tag.classList.remove("d-none");
-        }
-        return;
-    }
-
-    for (const tag of photos) {
-        if (tag.dataset.country !== e.target.dataset.country) {
-            tag.classList.add("d-none");
-            tag.classList.remove("d-block");
-        } else {
-            tag.classList.add("d-block");
-            tag.classList.remove("d-none");
-        }
-    }
-}
 
 function removeCard() {
     let card = document.querySelector(`.bgCard`);
@@ -83,44 +58,18 @@ function Destination() {
                     sentence={"Popular Destination"}
                 />
                 <div className="container">
-                    <div className="belt d-flex justify-content-evenly flex-wrap gap-4">
-                        <button
-                            onClick={show}
-                            data-country="all"
-                            className="rounded-pill p-2 active">
-                            All
-                        </button>
-                        <button
-                            onClick={show}
-                            data-country="egypt"
-                            className="rounded-pill p-2">
-                            Egypt
-                        </button>
-                        <button
-                            onClick={show}
-                            data-country="china"
-                            className="rounded-pill p-2">
-                            China
-                        </button>
-                        <button
-                            onClick={show}
-                            data-country="europe"
-                            className="rounded-pill p-2">
-                            Europe
-                        </button>
-                        <button
-                            onClick={show}
-                            data-country="canada"
-                            className="rounded-pill p-2">
-                            Canada
-                        </button>
-                        <button
-                            onClick={show}
-                            data-country="singapore"
-                            className="rounded-pill p-2">
-                            Singapore
-                        </button>
-                    </div>
+                    <ButtonsBelt
+                        buttons={[
+                            { content: "All", attr: "all" },
+                            { content: "Egypt", attr: "egypt" },
+                            { content: "China", attr: "china" },
+                            { content: "Europe", attr: "europe" },
+                            { content: "Canada", attr: "canada" },
+                            { content: "Singapore", attr: "singapore" },
+                        ]}
+                        editClasses={".photos .image"}
+                        tagsDataSet={"country"}
+                    />
                     <div className="photos row my-4 justify-content-center align-items-center">
                         {images.map((img, index) => (
                             <div
