dbc5d3ffd5915a464966bd6ce971828965ace213
unknown <youssef.eladawy1006@gmail.com>
2025-11-14 15:20:24 +0200

    Added Header, Footer, and Subscription components, and Added Hero and 404Error Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import "@popperjs/core/dist/umd/popper.min.js";
-import "bootstrap-icons/font/bootstrap-icons.css";   
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 import Testimonial from "./pages/Testimonial";
 import AboutUs from "./pages/AboutUs";
@@ -12,24 +12,33 @@ import Guides from "./pages/Guides";
 import Gallery from "./pages/Gallery";
 import Destination from "./pages/Destination";
 import Contact from "./pages/Contact";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
+import Subscription from "./components/Subscription";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import PageNotFound from "./pages/404Page";
 
-function App(){
-	return(
-		<>
-			<Routes >
-				<Route path="/" element={<Home/>} />
-				<Route path="/contact" element={<Contact/>} />
-				<Route path="/destination" element={<Destination />} />
-				<Route path="/testimonial" element={<Testimonial />} />
-				<Route path="/about" element={<AboutUs />} />
-				<Route path="/services" element={<OurServices />} />
-				<Route path="/guides" element={<Guides />} />
-				<Route path="/gallery" element={<Gallery/>} />
-			</Routes>
-			
-		</>
-	)
+function App() {
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/testimonial" element={<Testimonial />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/services" element={<OurServices />} />
+        <Route path="/guides" element={<Guides />} />
+        <Route path="/gallery" element={<Gallery />} />
+
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Subscription />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
